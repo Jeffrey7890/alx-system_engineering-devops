2db11b8a984536408075301dcfa5ed6530bc6652
= <=>
2024-08-13 17:05:18 -0700

    foc string

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,6 +2,7 @@ import requests
 
 
 def number_of_subscribers(subreddit):
+    """ get tge number of sub of s subreddit """
     headers = {
         'User-Agent': '''Mozilla/5.0 (Windows NT 10.0; Win64; x64;
                     rv:92.0) Gecko/20100101 Firefox/92.0'''
